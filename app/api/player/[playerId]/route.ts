be965c90dc0287173251169f80f88716081e301d
Joakim-Jobloop <Joakim@jobloop.no>
2025-04-09 11:55:11 +0200

    removed unused code in api route

--- app/api/player/[playerId]/route.ts
+++ app/api/player/[playerId]/route.ts
@@ -1,45 +1,8 @@
 // import { MongoClient, ServerApiVersion } from "mongodb";
 import { NextResponse, NextRequest } from "next/server";
 
-// if (!process.env.MONGODB_URI) {
-//   throw new Error("MONGODB_URI environment variable is not set");
-// }
-
-// const mongoClient = new MongoClient(process.env.MONGODB_URI?.toString(), {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// export async function GET(
-//   req: NextRequest,
-//   {params} : { params: { playerId: string } }
-// ) {
-//   const playerId  = params.playerId;
-//   try {
-//     await mongoClient.connect();
-//     const db = mongoClient.db("eldoria-updating-database");
-//     const player = await db.collection("players").findOne({ playerId });
-
-//     if (!player) {
-//       return NextResponse.json({ error: "Player not found" }, { status: 404 });
-//     }
-//     console.log("player found! Getting...")
-//     return NextResponse.json(player);
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: "Failed to fetch player", details: error },
-//       { status: 500 }
-//     );
-//   } finally {
-//     await mongoClient.close();
-//   }
-// }
-
 // post a check to the server:
- export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   return NextResponse.json(body);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
-// import Link from "next/link";
-// import Image from "next/image";
-
-// import Image from "next/image";
-// import { sanityFetch } from "./sanity/live";
-// import { allItems } from "./sanity/items";
-
 export default async function Home() {
-  // const { data } = await sanityFetch({query: allItems});
   return (
     <>
       <section
@@ -14,33 +6,9 @@ export default async function Home() {
         {/* {loading && <span>Loading...</span>} */}
 
         <h1 className={`text-9xl`}>Eldoria game</h1>
-        {/* <div className="flex flex-wrap">
-        {data?.map((item) => (
-          <div className="" key={item._id}>
-            <h2>{item.name}</h2>
-            <Image src={item.imageUrl} alt={item.name} width={250} height={250} />
-            <p>Type: {item.itemType?.join(", ")}</p>
-            <p>Subtype: {item.subType?.join(", ")}</p>
-            <p>Buy Price: {item.buyPrice}</p>
-            <p>Sell Price: {item.sellPrice}</p>
-          </div>
-        ))}
-        </div> */}
-        {/* <Link
-          className="h-32 w-32"
-          href="/shops/potionShop">
-          <Image
-            className="w-full h-full bg-black/30"
-            src="/images/potionShop/background/potion-shop.webp"
-            width={1920}
-            height={1080}
-            alt="Potion Shop"
-          />
-        </Link>
-        <span>Potion Shop</span> */}
+
         <p>use the compass in the top left corner to check out locations!</p>
       </section>
     </>
   );
 }
-// public\images\potionShop\background\potion-shop.webp
